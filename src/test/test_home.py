fb1abeca8e225253429bfc215a723b067ab8ba88
affan <mohammadaffan777@gmail.com>
2022-08-07 20:27:12 +0530

    its new changes

--- src/test/test_home.py
+++ src/test/test_home.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from src.pages.Homepage import HomePage
 from src.pages.Utilities import Utilities
@@ -10,14 +11,21 @@ class Test_Home(Test_Base):
     abc = Service("./src/test/chromedriver.exe")
     driver = webdriver.Chrome(service=abc)
 
+
     @pytest.fixture()
     def setup(self):
         self.homepage = HomePage(self.driver)
 
     @pytest.mark.test1
     def test_click_On_Aboutpage(self,setup):
-        self.homepage.click_on_about_page()
-        assert self.homepage.get_element_text(), "pass hogaya"
+         self.homepage.click_on_about_page()
+         print("===================")
+
+         #for printing someting from print function -s
+         self.homepage.get_element_text()
+         print("===================")
+
+
 
     @pytest.mark.test2
     def test_click_On_allcourses(self,setup):
